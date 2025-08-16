7e04660082ea7814dc64748ff1f74a7170098a49
Dongju Park <cxz541@gmail.com>
2025-08-16 16:51:12 +0900

    feat(collect): collect & send metrics

---
+++ agent/agent.go
@@ -0,0 +1,48 @@
+package agent
+
+import (
+	"context"
+	"log"
+	"morpher-agent/internal/client"
+	"morpher-agent/internal/collector"
+	"time"
+)
+
+type Sender interface {
+	Send(ctx context.Context, payload any) error
+}
+
+type Agent struct {
+	sender Sender
+}
+
+func New(sender Sender) *Agent {
+	return &Agent{sender: sender}
+}
+
+func Run(ctx context.Context, serverURL string) error {
+	sender := client.NewHTTPClient(serverURL, 10*time.Second)
+	agent := New(sender)
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	if _, err := collector.CollectSystem(); err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-ticker.C:
+			info, err := collector.CollectSystem()
+			if err != nil {
+				log.Printf("failed to collect system info: %v", err)
+			}
+			if err := agent.sender.Send(ctx, info); err != nil {
+				log.Printf("failed to send system info: %v", err)
+			}
+		}
+	}
+}
